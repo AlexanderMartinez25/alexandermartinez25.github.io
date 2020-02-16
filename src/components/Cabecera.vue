bb168969b814538fb57cbb06a6a0df72c159313b
Alexander Martinez <amartinez.byob@gmail.com>
2020-02-15 22:35:37 -0300

    Revert "GitHub page"

---
+++ src/components/Cabecera.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="text-cen">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <h1>Frutas</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Cabecera"
+};
+</script>
---
+++ src/views/Servicios.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="mt-5">
+    <h1>Servicios</h1>
+  </div>
+</template>
